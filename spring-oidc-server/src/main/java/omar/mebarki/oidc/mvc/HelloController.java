c78c1cfd68bd41e251191bdefedc294dbd265bf5
Omar MEBARKI <omar.mebarki@gmail.com>
2019-08-08 23:53:57 +0200

    oAuth2 with keycloak

--- spring-oidc-server/src/main/java/omar/mebarki/oidc/mvc/HelloController.java
+++ spring-oidc-server/src/main/java/omar/mebarki/oidc/mvc/HelloController.java
@@ -2,15 +2,20 @@ package omar.mebarki.oidc.mvc;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class HelloController {
 
-    @GetMapping("/me")
+    @GetMapping("/me1")
     public ResponseEntity<OAuth2AuthenticationToken> hello(OAuth2AuthenticationToken currentUser) {
         return ResponseEntity.ok(currentUser);
     }
 
+    @GetMapping("/me")
+    public ResponseEntity<OAuth2Authentication> hello(OAuth2Authentication currentUser) {
+        return ResponseEntity.ok(currentUser);
+    }
 }
