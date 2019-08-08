2b858b163fd89554754d35b9b4b73d7fc1cfe0f5
Omar MEBARKI <omar.mebarki@gmail.com>
2019-08-08 23:56:33 +0200

    oAuth2 with keycloak

--- spring-oidc-server/src/main/java/omar/mebarki/oidc/mvc/config/ResourceServerConfig.java
+++ spring-oidc-server/src/main/java/omar/mebarki/oidc/mvc/config/ResourceServerConfig.java
@@ -6,6 +6,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
+/**
+ * important @EnableResourceServer
+ */
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
